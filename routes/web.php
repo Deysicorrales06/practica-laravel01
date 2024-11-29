384d9585054e0796c918a3d507d02611a40b215e
Deysicorrales06 <76816143@idexperujapon.edu.pe>
2024-11-29 17:15:43 -0500

    Primer Commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\Controller76816143;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+route::get('/76816143',[controller76816143::class,'index']);
